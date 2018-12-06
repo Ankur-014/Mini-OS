edd2f7325ecd4173b3bf8579ef151199989cb4c8
Salim Shadman Ankur <31352360+Ankur-014@users.noreply.github.com>
2018-12-06 17:31:43 +0600

    Added logout

--- kernel.c
+++ kernel.c
@@ -2,19 +2,17 @@
 #include "include/kb.h"
 #include "include/string.h"
 #include "include/substr.h"
-#include "include/strint.h"
-
 kmain()
 {
        clearScreen();
 	   
-       print("Welcome to my operating system(1507014)!\nUser login\n");
+       print("Welcome to my operating system(1507014)!\n");
+ A:    print("User login\n");
 	   string username = "ankur";
-	   string password = "1507014";
+	   string password = "123";
 	   
 	   string inp = 0;
 	   string in = 0;
-	   string in2 = 0;
 	   while (1) {
 		   uint8 uOK = 0, pOK = 0;
 		   
@@ -27,10 +25,10 @@ kmain()
 		   if (strEql(inp, password)) pOK = 1;
 		   
 		   if (uOK && pOK) {
-			   print("Logged in as ankur!\n");
+			   print("Login Done!\n");
 			   break;
 		   }
-		   print("Wrong information. Try again\n");
+		   print("Wrong info. Try again\n");
 	   }
 	   
        while (1)
@@ -41,80 +39,16 @@ kmain()
 			{
 					print("You are already in cmd\n");
 			}
-			else if(strEql(inp,"cls"))
+			else if(strEql(inp,"clear"))
 			{
 					clearScreen();
-			}
-			else if(strEql(inp,"len"))
-			{
-				print("Enter string: ");
-				inp = readStr();
-				print("The length of string is: ");
-				printnum(strlength(inp));
-				print("\n");
-			}
-			else if(strEql(inp,"cll"))
-			{
-					clearLine(0,1);
-			}
-			else if(strEql(inp,"cline"))
-			{
-				print("Enter line no: ");
-				inp = readStr();
-				uint8 lineno = (uint8) strToInt(inp, 999);
-				if (lineno < 1 || lineno > cursorY) {
-					print("Line no is out of screen\n");
-					continue;
-				}
-				clearLine(lineno-1, lineno);
-				uint8 i,j;
-				string vidmem = (string)0xb8000; 
-				for (i = lineno-1 +1; i<=cursorY; i++) {
-					for (j = 0; j<sw; j++) {
-						vidmem[((i-1)*sw+j)*2] = vidmem[(i*sw+j)*2];
-						vidmem[((i-1)*sw+j)*2+1] = vidmem[(i*sw+j)*2+1];
-					}
-				}
-				cursorY--;
-				updateCursor();
-			}
-			else if(strEql(inp,"scr"))
-			{
-					scrollUp(2);
-			}
-			else if(strEql(inp,"tb"))
-			{
-					printch(0x09);
-			}
-			else if(strEql(inp,"bc"))
-			{
-					printch(0x08);
-			}
-			else if(strEql(inp,"new"))
-			{
-					newLineCheck();
-			}
-			else if(strEql(inp,"upc"))
-			{
-				print("Enter X: ");
-				in = readStr();
-				print("Enter Y: ");
-				 in2 = readStr();
-				//uint8 lineno = (uint8) strToInt(inp, 999);
-				cursorX=(uint8) strToInt(in, 999);;
-				cursorY=(uint8) strToInt(in2, 999);;
-				updateCursor();
 			}
 			else if(strEql(inp,"ankur"))
 			{
 					print("Username\n");
 			}
-			else if(strEql(inp,"os"))
-			{
-					print("CSE 3202\n");
-			}
 			
-			else if(strEql(inp,"sub"))
+			else if(strEql(inp,"substr"))
 			{
 				print("Enter string: ");
 				inp = readStr();
@@ -122,7 +56,14 @@ kmain()
 			}
 			else if(strEql(inp,"logout"))
 			{
+					clearScreen();
 					print("Successfully logout!\n");
+					goto A;
+					break;
+			}
+			else if(strEql(inp,"shut"))
+			{
+					print("Shutdown Successfully!\n");
 					break;
 			}
 			else
